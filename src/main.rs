07684440aab4fd23924e5542bfa41894b685d206
Brandon Liu <bdon@bdon.org>
2020-02-14 12:56:27 -0500

    start parsing moedict

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use serde::{Deserialize};
+use serde_json::{Result, Value};
+use std::fs;
+
+#[macro_use]
+extern crate serde_derive;
+
+#[derive(Deserialize)]
+struct Entry {
+    title:String,
+    heteronyms:Vec<Value>
+}
+
 fn main() {
-    println!("Hello, 世界");
+    let args: Vec<String> = env::args().collect();
+    let contents = fs::read_to_string(&args[1]).expect("should open file");
+    let entries: Vec<Entry> = serde_json::from_str(&contents).expect("file should be proper JSON");
+
+    for entry in &entries {
+        if entry.title.chars().count() == 1 && entry.heteronyms.len() == 1 {
+            println!("{} {}",entry.title, entry.heteronyms.len());
+        }
+    }
 }
