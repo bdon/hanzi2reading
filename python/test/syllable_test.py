5702e7801113fbe4e9109e3013f36cb5f7e07f67
Brandon Liu <bdon@bdon.org>
2020-06-28 17:01:20 +0800

    implement syllable to pinyin

--- python/test/syllable_test.py
+++ python/test/syllable_test.py
@@ -67,6 +67,64 @@ def to_zhuyin(s):
         z += '儿'
     return z
 
+P_INITIALS = ['b','p','m','f','d','t','n','l','g','k','h','j','q','x','zh','ch','sh','r','z','c','s']
+P_MEDIALS = ['i','u','ü']
+P_FINALS = ['a','o','e','ie','ai','ei','ao','ou','an','en','ang','eng']
+P_NO_INITIAL = [
+    [  '', 'a', 'o','e',  'ê', 'ai', 'ei', 'ao',' ou',  'an', 'en', 'ang', 'eng'],
+    ['yi','ya',  '', '', 'ye',   '',   '','yao','you', 'yan','yin','yang','ying'],
+    ['wu','wa','wo', '',   '','wai','wei',   '',   '', 'wan','wen','wang','weng'],
+    ['yu', '',   '', '','yue',   '',   '',   '',   '','yuan','yun',    '','yong']
+]
+
+# https://en.wikipedia.org/wiki/Pinyin_table
+def to_pinyin(s):
+    p = ''
+    if s.initial == 0:
+        p += P_NO_INITIAL[s.medial][s.final]
+    else:
+        p += P_INITIALS[s.initial-1]
+        if s.initial in [12,13,14] and s.medial == 3 and s.final == 0:
+            p += 'u'
+        elif s.initial in [12,13,14] and s.medial == 3 and s.final == 4:
+            p += 'ue'
+        elif s.initial in [12,13,14] and s.medial == 3 and s.final == 9:
+            p += 'uan'
+        elif s.initial in [12,13,14] and s.medial == 3 and s.final == 10:
+            p += 'un'
+        elif s.medial == 1 and s.final == 4:
+            p += 'ie'
+        elif s.medial == 1 and s.final == 8:
+            p += 'iu'
+        elif s.medial == 2 and s.final == 6:
+            p += 'ui'
+        elif s.medial == 2 and s.final == 10:
+            p += 'un'
+        else:
+            if s.medial > 0:
+                p += P_MEDIALS[s.medial-1]
+            if s.final > 0:
+                p += P_FINALS[s.final-1]
+
+    if 'a' in p:
+        p = p.replace('a','āáǎà'[s.tone - 1])
+    elif 'e' in p:
+        p = p.replace('e','ēéěè'[s.tone - 1])
+    elif 'o' in p:
+        p = p.replace('o','ōóǒò'[s.tone - 1])
+    elif 'iu' in p:
+        p = p.replace('iu','iū')
+    elif 'ui' in p:
+        p = p.replace('ui','uī')
+    elif 'i' in p:
+        p = p.replace('i','īíǐì'[s.tone - 1])
+    elif 'u' in p:
+        p = p.replace('u','ūúǔù'[s.tone - 1])
+    elif 'ü' in p:
+        p = p.replace('ü','ǖǘǚǜ'[s.tone - 1])
+
+    return p
+
 class TestSyllable(unittest.TestCase):
     def test_first_tone(self):
         s = from_zhuyin('ㄎㄧㄤ')
@@ -91,6 +149,32 @@ class TestSyllable(unittest.TestCase):
         self.assertEqual(len(b),2)
         self.assertEqual(b,b'\x29\xb2') # 0b 0010 1001 1011 0010
         self.assertEqual(to_zhuyin(Syllable.from_bytes(b)),'ㄎㄧㄤ')
+
+    def test_pinyin(self):
+        def zp(z,p):
+            self.assertEqual(to_pinyin(from_zhuyin(z)),p)
+
+        zp('ㄧ','yī')
+        zp('ㄧㄡ','yōu')
+        zp('ㄧㄣ','yīn')
+        zp('ㄧㄥ','yīng')
+        zp('ㄨ','wū')
+        zp('ㄨㄟ','wēi')
+        zp('ㄨㄣ','wēn')
+
+        zp('ㄋㄩ','nǖ')
+        zp('ㄐㄩ','jū')
+        zp('ㄐㄩㄝ','juē')
+        zp('ㄐㄩㄢ','juān')
+        zp('ㄐㄩㄣ','jūn')
+
+        zp('ㄐㄧㄝ','jiē')
+        zp('ㄌㄧㄡ','liū')
+        zp('ㄉㄨㄟ','duī')
+        zp('ㄉㄨㄣ','dūn')
+
+        zp('ㄎㄧㄤ','kiāng')
+        zp('ㄉㄨㄤ','duāng')
 
 if __name__ == '__main__':
     unittest.main()
