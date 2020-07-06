d2a7ca0792d256f9359815c8c203ebf304869c40
Brandon Liu <bdon@bdon.org>
2020-07-06 16:40:20 +0800

    finish Unihan db implementation

--- python/hanzi2reading/create_unihan.py
+++ python/hanzi2reading/create_unihan.py
@@ -1,4 +1,8 @@
 import sys
+from pinyin import parse
+from hanzi2reading.serialize import write
+
+entries = []
 
 with open(sys.argv[1],'r') as f:
     for l in f.readlines():
@@ -8,4 +12,14 @@ with open(sys.argv[1],'r') as f:
         if parts[1] == 'kMandarin':
             c = chr(int(parts[0][2:],16))
             pinyin = parts[2]
-            print(c, pinyin)
+            if ' ' in pinyin:
+               pinyin = pinyin.split(' ')[0] # prefer zh-hans (1 is zh-hant)
+            try:
+                entries.append((c,[parse(pinyin)]))
+            except Exception:
+                print("Ignored invalid pinyin:", c,pinyin)
+
+print("Total entries:", len(entries))
+
+with open(sys.argv[2],'wb') as f:
+    write(f,entries)
